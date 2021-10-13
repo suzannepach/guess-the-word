fa16649f42abfc89bde68f43868f00455fe95870
Suzanne-Pach <suzannepch@gmail.com>
2021-10-13 12:14:12 +0200

    Step 3 of 6: accept and validate player guesses

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "Magnolia";
+const guessedLetters = [];
 
 
 // Use symbols as palceholders for the letters of the word that's being guessed
@@ -24,7 +25,48 @@ placeholder(word);
 
 guessButton.addEventListener ("click", function (e) {
     e.preventDefault();
+    // Empty message paragraph
+    message.innerText = "";
+    // This grabs what was entered in the input
     const guess = letterInput.value;
-    console.log(guess);
+    // Checks input is only a single letter
+    const goodGuess = inputCheck(guess);
+
+    // We've got a letter, let's guess.
+    if (goodGuess) {
+        makeGuess(guess);
+    }
     letterInput.value = "";
-})
+});
+
+// This function's purpose is to validate the player’s input.
+const inputCheck = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    // Is input empty?
+    if (input.length === 0) {
+        message.innerText = "Please type a letter.";
+    }
+    // Is input more than 1?
+    else if (input.length > 1) {
+        message.innerText = "Please type only one letter.";
+    }
+    // Is input not a letter?
+    else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please only type a letter (A-Z or a-z).";
+    }
+    else {
+        // Result: only one single letter.
+        return input;
+    }
+};
+
+const makeGuess = function (letter) {
+    letter = letter.toUpperCase();
+    if (guessedLetters.includes(letter)) {
+        message.innerText = "You've guessed that letter already, please try another letter.";
+     }
+    else {
+        guessedLetters.push(letter);
+        console.log(guessedLetters);
+        }
+    };
